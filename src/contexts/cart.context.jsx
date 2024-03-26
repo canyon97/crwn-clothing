59f60d5ea5dd272bab820c93ad6c965be7c2874b
cpe342 <canyon@utexas.edu>
2024-03-25 20:21:42 -0500

    -Implement adding items to cart -Add in functionality to rerender cart icon count based on cart items -Add in tracking of cart items in dopdown

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,19 +1,58 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find((cartItem) => {
+    return cartItem.id === productToAdd.id;
+  });
+
+  // Existing product
+  if (existingCartItem) {
+    // We always want to return a new array - dont mutate the original
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  // New product
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 // Initialize the values portion of the state
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   // Get the state values
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
 
   // Create value that will be passed via context
   const value = {
     isCartOpen,
     setIsCartOpen,
+    cartItems,
+    addItemToCart,
+    cartCount,
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
